cd6d6c794aa14bf55ce4cc417df3ed42e4a6f1a0
softnero <ang.ma.martin@gmail.com>
2020-09-10 16:06:53 +0800

    add a prototype add function

--- cmd/add.go
+++ cmd/add.go
@@ -17,22 +17,17 @@ package cmd
 
 import (
 	"fmt"
-
+	"strings"
 	"github.com/spf13/cobra"
 )
 
 // addCmd represents the add command
 var addCmd = &cobra.Command{
 	Use:   "add",
-	Short: "A brief description of your command",
-	Long: `A longer description that spans multiple lines and likely contains examples
-and usage of using your command. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+	Short: "add a TO-DO thing to the task list",
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("add called")
+		res := strings.Join(args, " ")
+		fmt.Printf("Added \"%s\" to the task list",res)
 	},
 }
 
